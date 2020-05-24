534ef22d999868737bc3e9824c73ca603a7c41f6
Bui Chung <chungbd90@gmail.com>
2020-05-24 23:01:56 +0700

    add search behavior

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,22 @@
-import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:hnapp/bloc/hn_bloc.dart';
-import 'json_parsing.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
+import 'dart:collection';
 
-void main()  {
-   var blocProvider = BlocProvider(
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+import 'package:hnapp/bloc/hn_bloc.dart';
+
+import 'json_parsing.dart';
+
+void main() {
+  var blocProvider = BlocProvider(
     create: (context) => HnBloc(),
     child: MyApp(),
   );
   runApp(blocProvider);
 }
-class MyApp extends StatelessWidget {
 
+class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -67,16 +71,25 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: 
-        AppBar(
-          // Here we take the value from the MyHomePage object that was created by
-          // the App.build method, and use it to set our appbar title.
-          title: Text(widget.title),
-          leading: LoadingWidget(),
-        ),
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+        elevation: 0.0,
+        leading: LoadingWidget(),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () async {
+              var result =
+                  await showSearch(context: context, delegate: ArticleSearch());
+              if (result != null) {}
+            },
+          )
+        ],
+      ),
       body: RefreshIndicator(
-        child: 
-        BlocBuilder<HnBloc, HnState>(
+        child: BlocBuilder<HnBloc, HnState>(
           builder: (context, state) {
             return ListView(
               children: state.articles.map((i) => _buildItem(i)).toList(),
@@ -88,23 +101,22 @@ class _MyHomePageState extends State<MyHomePage> {
           setState(() {
             _articles.removeAt(0);
           });
-
         },
       ),
       bottomNavigationBar: BottomNavigationBar(
-              items: const <BottomNavigationBarItem>[
-                BottomNavigationBarItem(
-                  title: Text('Top Stories'),
-                  icon: Icon(Icons.arrow_drop_up),
-                ),
-                BottomNavigationBarItem(
-                  title: Text('New Stories'),
-                  icon: Icon(Icons.new_releases),
-                ),
-              ],
-              currentIndex: _selectedIndex,
-              onTap: _onItemTapped,
-            ),
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            title: Text('Top Stories'),
+            icon: Icon(Icons.arrow_drop_up),
+          ),
+          BottomNavigationBarItem(
+            title: Text('New Stories'),
+            icon: Icon(Icons.new_releases),
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+      ),
     );
   }
 
@@ -126,18 +138,19 @@ class _MyHomePageState extends State<MyHomePage> {
     return Padding(
       padding: const EdgeInsets.all(16.0),
       child: ExpansionTile(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                Text("${article.descendants} comments"),
-                IconButton(icon: Icon(Icons.launch), onPressed: () {
-
-                })
-              ],
-            )
-          ],
-        title: Text(article.title, style: TextStyle(fontSize: 20),),
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Text("${article.descendants} comments"),
+              IconButton(icon: Icon(Icons.launch), onPressed: () {})
+            ],
+          )
+        ],
+        title: Text(
+          article.title,
+          style: TextStyle(fontSize: 20),
+        ),
       ),
     );
   }
@@ -152,42 +165,109 @@ class LoadingWidget extends StatefulWidget {
   _LoadingWidgetState createState() => _LoadingWidgetState();
 }
 
-class _LoadingWidgetState extends State<LoadingWidget> with TickerProviderStateMixin {
+class _LoadingWidgetState extends State<LoadingWidget>
+    with TickerProviderStateMixin {
   AnimationController _controller;
 
   @override
   void initState() {
     super.initState();
-    _controller = AnimationController(
-      vsync: this,
-      duration: Duration(seconds: 1)
-      );
+    _controller =
+        AnimationController(vsync: this, duration: Duration(seconds: 1));
   }
 
   @override
   Widget build(BuildContext context) {
-    return 
-      BlocBuilder<HnBloc, HnState>(
-        builder: (context, state) {
-          // if (state.isLoading) {
-            _controller.forward().then((f) {
-              _controller.reverse();
-            });
-            // _controller.reverse();
-            return FadeTransition(
-              child: Icon(FontAwesomeIcons.hackerNews),
-              opacity: 
-                Tween(begin: 0.5, end: 1.0)
-                .animate(
-                  CurvedAnimation(
-                    curve: Curves.easeIn, 
-                    parent: _controller
-                    )
-                ),
-              );
-          // }
-          // return Container();
+    return BlocBuilder<HnBloc, HnState>(
+      builder: (context, state) {
+        // if (state.isLoading) {
+        _controller.forward().then((f) {
+          _controller.reverse();
+        });
+        // _controller.reverse();
+        return FadeTransition(
+          child: Icon(FontAwesomeIcons.hackerNews),
+          opacity: Tween(begin: 0.5, end: 1.0).animate(
+              CurvedAnimation(curve: Curves.easeIn, parent: _controller)),
+        );
+        // }
+        // return Container();
+      },
+    );
+  }
+}
+
+class ArticleSearch extends SearchDelegate<Article> {
+  final List<Article> articles;
+  ArticleSearch({
+    this.articles,
+  });
+
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    return [
+      IconButton(
+        icon: Icon(Icons.clear),
+        onPressed: () {
+          query = "";
         },
-      );
+      )
+    ];
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    return IconButton(
+      icon: Icon(Icons.arrow_back),
+      onPressed: () {
+        close(context, null);
+      },
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    return BlocBuilder<HnBloc, HnState>(
+      builder: (context, state) {
+        final results = state.articles.where((element) {
+          return element.title.toLowerCase().contains(query);
+        });
+
+        return ListView(
+          children: results
+              .map<Widget>((e) => ListTile(
+                    title: Text(e.title),
+                    leading: Icon(Icons.book),
+                    onTap: () {
+                      close(context, e);
+                    },
+                  ))
+              .toList(),
+        );
+      },
+    );
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    return BlocBuilder<HnBloc, HnState>(
+      builder: (context, state) {
+        final results = state.articles.where((element) {
+          return element.title.toLowerCase().contains(query);
+        });
+
+        return ListView(
+          children: results
+              .map<Widget>((e) => ListTile(
+                    title: Text(e.title),
+                    leading: Icon(Icons.book),
+                    onTap: () {
+                      query = e.title;
+                    },
+                  ))
+              .toList(),
+        );
+      },
+    );
   }
 }
